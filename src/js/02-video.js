a5c3ff4ffd2d4497d24ec11e7ead75cd7c8297e7
Serhii Tymchuk <92324923+eNgine9r@users.noreply.github.com>
2023-02-22 23:59:14 +0200

    Update 2 task / done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const getCurrentTime = ({ seconds }) => {
+  localStorage.setItem('videoplayer-current-time', seconds);
+};
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
